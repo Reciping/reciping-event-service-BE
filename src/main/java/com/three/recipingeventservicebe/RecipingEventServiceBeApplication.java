f4bb4efc8ffaa39fdc6f8d72f02bad4cd300be30
beginninggrace <tldms123455@naver.com>
2025-05-05 01:55:22 +0900

    :tada: first commit

---
+++ src/main/java/com/three/recipingeventservicebe/RecipingEventServiceBeApplication.java
@@ -0,0 +1,13 @@
+package com.three.recipingeventservicebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipingEventServiceBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipingEventServiceBeApplication.class, args);
+    }
+
+}
