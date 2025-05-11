fd138a7fc4b974b23a8f26d333c5132f6d334372
beginninggrace <tldms123455@naver.com>
2025-05-12 00:02:37 +0900

    :recycle: refactor : 어플리케이션 시작 클래스에 스케줄링 활성화

--- src/main/java/com/three/recipingeventservicebe/RecipingEventServiceBeApplication.java
+++ src/main/java/com/three/recipingeventservicebe/RecipingEventServiceBeApplication.java
@@ -2,8 +2,10 @@ package com.three.recipingeventservicebe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RecipingEventServiceBeApplication {
 
     public static void main(String[] args) {
